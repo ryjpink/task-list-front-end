473ce565887da321d6b770b22ff7abd51e183381
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-30 11:55:28 -0800

    Update delete button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
 const Task = (props) => {
-  const [complete, setComplete] = useState(props.isComplete);
-
- 
+  const [complete, setComplete] = useState(props.isComplete); 
   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
 
+  const remove = (id)=>{
+    const element = document.getElementById(id);
+    console.log(id);
+    element.remove();
+
+  }
+
   return (
-    <li className="tasks__item">
+    <li className="tasks__item" id={props.id}>
       <button
         className={`tasks__item__toggle ${buttonClass}`}
         onClick={() => setComplete(!complete)}
       >
         {props.title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" onClick={()=>{remove(props.id)}}>x</button>
     </li>
   );
 };
